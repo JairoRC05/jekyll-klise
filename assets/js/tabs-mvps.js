ae8024e918b3571987ab6c406fa0e9b7181410e2
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2025-05-31 17:46:08 -0600

    estadisticas

--- assets/js/tabs-mvps.js
+++ assets/js/tabs-mvps.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   const roundTabs = document.getElementById('roundTabs');
   const roundTabsContent = document.getElementById('roundTabsContent');
   const assetsFolder = 'assets/rondas/';
-  const numRondas = 12;
+  const numRondas = 13;
 
   async function leerJSON(nombreArchivo) {
     try {
