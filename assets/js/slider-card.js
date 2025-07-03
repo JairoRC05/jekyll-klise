f37e1c81a5c985ddcc83b1cc9f4254634a3974ca
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2025-07-03 14:31:05 -0600

    cards

--- assets/js/slider-card.js
+++ assets/js/slider-card.js
@@ -1,17 +1,17 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  await cargarEquipos(); 
 
+  const carrusel = document.getElementById("contenedor-cards");
+  const btnIzq = document.getElementById("slide-left");
+  const btnDer = document.getElementById("slide-right");
 
-    const carrusel = document.getElementById("contenedor-cards");
-    const btnIzq = document.getElementById("slide-left");
-    const btnDer = document.getElementById("slide-right");
+  const scrollBy = 300;
 
-    const scrollBy = 300; // Puedes ajustar según tamaño de card
+  btnIzq.addEventListener("click", () => {
+    carrusel.scrollBy({ left: -scrollBy, behavior: "smooth" });
+  });
 
-    btnIzq.addEventListener("click", () => {
-      carrusel.scrollBy({ left: -scrollBy, behavior: "smooth" });
-    });
-
-    btnDer.addEventListener("click", () => {
-      carrusel.scrollBy({ left: scrollBy, behavior: "smooth" });
-    });
-  });   
+  btnDer.addEventListener("click", () => {
+    carrusel.scrollBy({ left: scrollBy, behavior: "smooth" });
+  });
+});
